fd0929f7fc1ae1dd3a8e41fdc77f27e016562767
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-04-20 14:52:57 -0500

    Add files via upload

---
+++ js/proyectos.js
@@ -0,0 +1,14 @@
+/*DESARROLLADO POR FERNANDO JUAREZ ALDORICA*/
+function link00(){
+	//PROXIMAMENTE
+	//window.open('https://www.google.com');
+	alert('Próximamente más proyectos, vuelve pronto.')
+}
+function link01(){
+	//Web HOUSE OF COMIC
+	window.open('https://www.google.com');
+}
+function link02(){
+	//Web ADRIANA JUAREZ
+	window.open('https://www.google.com');
+}
