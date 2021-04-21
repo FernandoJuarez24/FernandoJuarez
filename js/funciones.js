34b63d17c41b04327b0ed2087822f515464e4234
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-04-21 13:26:07 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,43 @@
+/*DESARROLLADO POR FERNANDO JUAREZ ALDORICA*/
+function link00(){
+	//PROXIMAMENTE
+	//window.open('https://www.google.com');
+	alert('Próximamente más proyectos, vuelve pronto.')
+}
+function link01(){
+	//Web HOUSE OF COMIC
+	window.open('https://fernandojuarez24.github.io/HoCMX/');
+}
+function cambio01(){
+	document.getElementById('hoc').src='img/proyectos/01-01.jpg'
+}
+function regreso01(){
+	document.getElementById('hoc').src='img/proyectos/01.jpg'
+}
+function link02(){
+	//Web ADRIANA JUAREZ
+	window.open('https://fernandojuarez24.github.io/AdrianaJuarez/');
+}
+function cambio02(){
+	document.getElementById('aj').src='img/proyectos/02-02.jpg'
+}
+function regreso02(){
+	document.getElementById('aj').src='img/proyectos/02.jpg'
+}
+
+//PORTAFOLIOS
+function alerta(){
+	alert('Sección en contrucción.')
+}
+//ABOUT
+function cambioAbout(){
+	document.getElementById('aboutImg').src='img/about-01.jpg'
+}
+function regresoAbout(){
+	document.getElementById('aboutImg').src='img/about.jpg'
+}
+//CONTACTO
+function mail(){
+	window.open('mailto:fernandojuareza24@gmail.com');
+
+}
