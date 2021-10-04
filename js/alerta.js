47b7380b88351301f42451fbdd82520f4dab8209
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-10-04 16:13:56 -0500

    Add files via upload

--- js/alerta.js
+++ js/alerta.js
@@ -1,18 +1,3 @@
 document.getElementById('alerta').onclick = function(){
     alert('Funcion en estado Beta, vuelve más tarde');
 }
-document.getElementById('alerta2').onclick = function(){
-    alert('Funcion en estado Beta, vuelve más tarde');
-}
-document.getElementById('alerta3').onclick = function(){
-    alert('Funcion en estado Beta, vuelve más tarde');
-}
-document.getElementById('alerta4').onclick = function(){
-    alert('Funcion en estado Beta, vuelve más tarde');
-}
-document.getElementById('alerta5').onclick = function(){
-    alert('Funcion en estado Beta, vuelve más tarde');
-}
-document.getElementById('alerta6').onclick = function(){
-    alert('Funcion en estado Beta, vuelve más tarde');
-}
---
+++ js/proyectos.js
@@ -0,0 +1,20 @@
+/* HOUSE OF COMIC */
+document.getElementById('HoC').onclick = function(){
+    window.open('https://fernandojuarez24.github.io/HoCMX/');
+}
+/* ADRIANA */
+document.getElementById('AJ').onclick = function(){
+   window.open('https://fernandojuarez24.github.io/AdrianaJuarez/');
+}
+/* PANADERIA */
+document.getElementById('pan').onclick = function(){
+   window.open('https://fernandojuarez24.github.io/LaTradicional/');
+}
+/* AUTOS */
+document.getElementById('auto').onclick = function(){
+   window.open('https://fernandojuarez24.github.io/MiCarcacha/');
+}
+/* TENIS */
+document.getElementById('tenis').onclick = function(){
+   window.open('https://fernandojuarez24.github.io/AirMexican/');
+}
